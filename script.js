f1a2d550dbeeceba7ce589b737f351c531fc1fb9
Ely Bourland <JEBourland@yahoo.com>
2021-01-16 19:04:21 -0800

    toggle button fucntion

---
+++ script.js
@@ -0,0 +1,18 @@
+/* 
+
+1 - Bring in toggle buttons (querySelectorAll)
+2 - Loop through nodeList (forEach)
+3 - Add click event (addEventListener)
+4 - Toggle the active class on the parent node (.parentNode & classList.toggle())
+
+*/
+
+const toggle = document.querySelectorAll('.faq-toggle');
+
+toggle.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active');
+    });
+});
+
+
